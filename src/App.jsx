241721323713ad5f0ce8798902b18c6ce8f054af
Leandro Motta <studioup77@gmail.com>
2026-08-10 18:36:10 -0300

    Remove diagnostico temporario

--- src/App.jsx
+++ src/App.jsx
@@ -4192,9 +4192,7 @@ export default function App(){
   const salvarEditProf=async(profAtualizado)=>{
     try{
       await salvarProfissional(profAtualizado.id, profAtualizado);
-      alert("DIAGNOSTICO: Salvo com sucesso! id="+profAtualizado.id+" pagamentoMisto="+profAtualizado.pagamentoMisto);
     }catch(e){
-      alert("DIAGNOSTICO: ERRO ao salvar! "+e.message+" | code="+(e.code||"sem codigo"));
       console.error("Erro ao salvar edição do profissional:", e);
     }
     if(currentUser?.id===profAtualizado.id) setCurrentUser(profAtualizado);
